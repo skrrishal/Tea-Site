3eb52fd1c8a8add39dd74c5f00662a36ae08ac75
Skrrishal <sagarkumar171020@gmail.com>
2024-06-22 22:28:23 +0530

    Change in color of  name text in footer

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
         <footer className='bg-[#322278] text-white flex justify-center items-center px-6 h-[3rem]'>
                 <div className='flex gap-2 text-center'>
                     <span>CopyRight ©</span>
-                    <span className='font-bold text-[17px] text-[#7eff51]'>Sagar-ITCS</span>
+                    <span className='font-bold text-[17px] text-[#3e8dfb]'>Sagar-ITCS</span>
                     <span>| All Rights Reserved</span>
                 </div>
         </footer>
